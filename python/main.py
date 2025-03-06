25a43beea6454ee50724da34dc24a546e2182538
Atsuko Uemura <atsukof.kt@gmail.com>
2025-03-05 21:11:20 -0800

    Complete step4-4

--- python/main.py
+++ python/main.py
@@ -1,13 +1,14 @@
 import os
 import logging
 import pathlib
-from fastapi import FastAPI, Form, HTTPException, Depends
+from fastapi import FastAPI, Form, HTTPException, Depends, File, UploadFile
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 import sqlite3
 from pydantic import BaseModel
 from contextlib import asynccontextmanager
 import json
+import hashlib
 
 
 # Define the path to the images & sqlite3 database
@@ -68,18 +69,25 @@ class AddItemResponse(BaseModel):
 
 # add_item is a handler to add a new item for POST /items .
 @app.post("/items", response_model=AddItemResponse)
-def add_item(
+async def add_item(
     name: str = Form(...),
     category: str = Form(...),
     db: sqlite3.Connection = Depends(get_db),
+    image: UploadFile = File(...)
 ):
     if not name:
         raise HTTPException(status_code=400, detail="name is required")
 
     if not category:
         raise HTTPException(status_code=400, detail="category is required")
+    
+    file_content = await image.read()
+    hash_name = hashlib.sha256(file_content).hexdigest()
+    image_path = images / f"{hash_name}.jpg"
+    with open(image_path, "wb") as f:
+        f.write(file_content)
 
-    insert_item(Item(name=name, category=category))
+    insert_item(Item(name=name, category=category, image=f"{hash_name}.jpg"))
     return AddItemResponse(**{"message": f"item received: {name}"}) 
 
 
@@ -114,11 +122,12 @@ async def get_image(image_name):
 class Item(BaseModel):
     name: str
     category: str
+    image: str
 
 
 def insert_item(item: Item):
     # STEP 4-2: add an implementation to store an item
-    item_dict = {"name": item.name, "category": item.category}
+    item_dict = {"name": item.name, "category": item.category, "image": item.image}
 
     if os.path.exists("items.json"):
         with open("items.json", "r") as f:
