e3e4580e1e130fd01f70e591c4fa3d7a7c1990c7
Atsuko Uemura <atsukof.kt@gmail.com>
2025-03-12 22:38:56 -0700

    Show item images in ItemList.tsx

--- typescript/simple-mercari-web/src/components/ItemList.tsx
+++ typescript/simple-mercari-web/src/components/ItemList.tsx
@@ -3,13 +3,16 @@ import { Item, fetchItems } from '~/api';
 
 const PLACEHOLDER_IMAGE = import.meta.env.VITE_FRONTEND_URL + '/logo192.png';
 
+
 interface Prop {
   reload: boolean;
   onLoadCompleted: () => void;
 }
 
+
 export const ItemList = ({ reload, onLoadCompleted }: Prop) => {
   const [items, setItems] = useState<Item[]>([]);
+
   useEffect(() => {
     const fetchData = () => {
       fetchItems()
@@ -34,7 +37,7 @@ export const ItemList = ({ reload, onLoadCompleted }: Prop) => {
         return (
           <div key={item.id} className="ItemList">
             {/* TODO: Task 2: Show item images */}
-            <img src={PLACEHOLDER_IMAGE} />
+            <img src={`http://localhost:9000/images/${item.image_name}`} />
             <p>
               <span>Name: {item.name}</span>
               <br />
